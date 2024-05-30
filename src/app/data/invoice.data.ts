e472ebff0483105b7ea82e5270ca27b560fe71f9
eljulio7410 <kaiser7420@gmail.com>
2024-05-30 11:32:32 -0500

    maquetacion creacion de objetos

---
+++ src/app/data/invoice.data.ts
@@ -0,0 +1,40 @@
+import { Invoice } from "../models/invoice";
+
+export const invoiceData: Invoice = {
+    id: 1,
+    name: 'Componentes PC',
+    client: {
+        name: 'Andres',
+        lastname: 'Doe',
+        address:{
+            country: 'USA',
+            city: 'Los Angeles',
+            street: 'One Street',
+            number: 15,
+        }
+    },
+    company: {
+        name: 'New age',
+        fiscalNumber: 32132164,
+    },
+    items: [
+        {
+            id: 1,
+            product: 'Cpu Intel i9',
+            price: 599,
+            quantity: 1
+        },
+        {
+            id: 2,
+            product: 'Teclado',
+            price: 399,
+            quantity: 2
+        },
+        {
+            id: 3,
+            product: 'Monitor',
+            price: 899,
+            quantity: 3
+        },
+    ]
+}
